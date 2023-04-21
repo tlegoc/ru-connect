d95cf9e77f84a3d3cc976f3981224ad72fb3375b
tlegoc <theo@legoc.net>
2023-04-21 11:08:31 +0200

    Changed layout, allow for different mobile/desktop containers

---
+++ src/lib/DesktopLayout.svelte
@@ -0,0 +1,2 @@
+<div id="desktop" />
+<slot />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,36 @@
 <script>
+	import DesktopLayout from '$lib/DesktopLayout.svelte';
+	import MobileLayout from '$lib/MobileLayout.svelte';
 	import './styles.css';
+
+	/**
+	 * @type {number}
+	 */
+	let innerWidth;
 </script>
 
+<svelte:window bind:innerWidth={innerWidth} />
+
 <div class="app">
+	{#if innerWidth > 600}
+		<DesktopLayout>
+			<slot />
+		</DesktopLayout>
+	{:else}
+		<MobileLayout>
+			<slot />
+		</MobileLayout>
+	{/if}
 </div>
 
 <style>
 	.app {
 		display: flex;
 		flex-direction: column;
-		min-height: 100vh;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		right: 0px;
+		bottom: 0px;
 	}
 </style>
